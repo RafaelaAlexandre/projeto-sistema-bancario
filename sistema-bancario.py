34ab9b127a24bdf3610c32636a53a7eb37ba8e58
RafaelaAlexandre <rafaela.a.oliveira1998@gmail.com>
2024-04-27 23:23:36 -0300

    :recycle: refactor: Refatorando o menu de operações

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -4,20 +4,24 @@ menu = """
 
     Escolha a operação desejada:
 
-    [1] SAQUE
-    [2] DEPOSITO
+    [1] DEPOSITO
+    [2] SAQUE
     [3] EXTRATO
     [0] SAIR
 
 """
 
+LIMITE_VALOR_SAQUE=500.00
+LIMITE_QUANT_SAQUE=3
+quant=0
+
 while True:
     opcao=int(input(menu+"==>>"))
 
     if opcao == 1:
-        print("Voce escolheu a opção: SAQUE")
-    elif opcao == 2:
         print("Voce escolheu a opção: DEPOSITO")
+    elif opcao == 2:
+        print("Voce escolheu a opção: SAQUE")
     elif opcao == 3:
         print("Voce escolheu a opção: EXTRATO")
     elif opcao == 0:
