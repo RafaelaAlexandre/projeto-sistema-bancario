7cd0de1b8207af8080b649155a45964beb47e9f9
RafaelaAlexandre <rafaela.a.oliveira1998@gmail.com>
2024-04-27 23:48:07 -0300

    :sparkles: feat: Adicionando operação deposito

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -14,12 +14,20 @@ menu = """
 LIMITE_VALOR_SAQUE=500.00
 LIMITE_QUANT_SAQUE=3
 quant=0
+saldo=0
+extrato=""
 
 while True:
     opcao=int(input(menu+"==>>"))
 
     if opcao == 1:
         print("Voce escolheu a opção: DEPOSITO")
+        deposito=float(input("entre com o valor que deseja depositar: "))
+        if deposito > 0:
+            saldo+=deposito
+            extrato+= f"Deposito: R$ {deposito:.2f}\n"
+        else:
+            print("valor inválido para deposito")   
     elif opcao == 2:
         print("Voce escolheu a opção: SAQUE")
     elif opcao == 3:
