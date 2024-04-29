6a9ebb49a005d10b665ecc3046132114e8a67d74
RafaelaAlexandre <rafaela.a.oliveira1998@gmail.com>
2024-04-28 23:18:31 -0300

    :sparkles: feat: Adicionando função menu de operações

---
+++ sistema-bancario-v2.py
@@ -0,0 +1,14 @@
+def menu_operacao():
+   menu = """
+
+    Escolha a operação desejada:
+
+    [1] DEPOSITO
+    [2] SAQUE
+    [3] EXTRATO
+    [0] SAIR
+
+    """ 
+   return int(input(menu+"==>"))
+
+    
